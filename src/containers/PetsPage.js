866c583443f3d27fba0bb2c9ae2e44aa6da12e29
tpstar <hanlee12@gmail.com>
2017-11-19 07:35:09 -0600

    done

--- src/containers/PetsPage.js
+++ src/containers/PetsPage.js
@@ -13,8 +13,15 @@ class PetsPage extends Component {
   }
 
   render() {
+    const {pets, match} = this.props;
     return (
-      <div>Pets Page</div>
+      <div>
+        <PetsList pets={pets} />
+        <Switch>
+          <Route path={`${match.url}/new`} component={PetsNew} />
+          <Route path={`${match.url}/:petId`} component={PetsShow} />
+        </Switch>
+      </div>
     )
   }
 };
